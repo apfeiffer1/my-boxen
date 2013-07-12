0a63d1a63e7020e9ce49cbeff28debd35c02ebc7
Giulio Eulisse <giulio.eulisse@gmail.com>
2013-07-13 01:29:10 +0200

    tmux.conf

--- modules/people/manifests/ktf.pp
+++ modules/people/manifests/ktf.pp
@@ -1,7 +1,21 @@
 class people::ktf {
 notify { 'class people::ktf declared': }
-  repository { "/Users/${my_username}/dotfiles":
+  $home     = "/Users/${::luser}"
+
+  repository { "${home}/dotfiles":
     source => 'ktf/dotfiles',
   }
 
+  file { "${home}/.zshrc":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${home}/dotfiles/zshrc",
+    require => Repository["${home}/dotfiles"],
+  }
+  file { "${home}/.tmux.conf":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${home}/dotfiles/tmux.conf",
+    require => Repository["${home}/dotfiles"],
+  }
 }
