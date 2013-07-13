b1d36c463435b01848ba409d8787f73cec368f37
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-07-13 03:13:38 +0200

    Git config.

--- modules/people/manifests/ktf.pp
+++ modules/people/manifests/ktf.pp
@@ -30,4 +30,12 @@ notify { 'class people::ktf declared': }
     target  => "${home}/dotfiles/vimrc",
     require => Repository["${home}/dotfiles"],
   }
+  
+  git::config::global { 'user.email': value  => 'giulio.eulisse@cern.ch'}
+  git::config::global { 'user.name': value  => 'Giulio Eulisse'}
+  git::config::global { 'user.github': value  => 'ktf'}
+  git::config::global { 'user.signingkey': value  => '03612AC1'}
+  git::config::global { 'color.interactive': value  => 'true'}
+  git::config::global { 'color.diff': value  => 'true'}
+  git::config::global { 'alias.graph': value  => "log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(red)%s%C(reset) %C(bold green)— %an%C(reset)%C(bold blue)%d%C(reset)' --abbrev-commit"}
 }
