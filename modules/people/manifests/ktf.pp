c2824dde76595d41cc73df1e0cdf899a6386af14
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-08-02 21:37:09 +0200

    Cleanup.

--- modules/people/manifests/ktf.pp
+++ modules/people/manifests/ktf.pp
@@ -1,5 +1,5 @@
 class people::ktf {
-notify { 'class people::ktf declared': }
+  notify { 'class people::ktf declared': }
   $home     = "/Users/${::luser}"
 
   repository { "${home}/dotfiles":
@@ -36,6 +36,7 @@ notify { 'class people::ktf declared': }
   git::config::global { 'user.email': value  => 'giulio.eulisse@cern.ch'}
   git::config::global { 'user.name': value  => 'Giulio Eulisse'}
   git::config::global { 'user.github': value  => 'ktf'}
+  git::config::global { 'github.user': value  => 'ktf'}
   git::config::global { 'user.signingkey': value  => '03612AC1'}
   git::config::global { 'alias.graph': value  => "log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(red)%s%C(reset) %C(bold green)— %an%C(reset)%C(bold blue)%d%C(reset)' --abbrev-commit"}
 }
